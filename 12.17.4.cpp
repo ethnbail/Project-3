9268f7f176487b0891c20f70b464c2d9934f5eda
ethnbail <findeab@gmail.com>
2024-10-23 19:49:17 -0700

    12.17.4.cpp

--- 12.17.4.cpp
+++ 12.17.4.cpp
@@ -1,5 +1,17 @@
-// Programmer - Ethan Bailey
-// Tester - Ethan Bailey
+/**
+      Project id: Group Project C 
+      Project Name: Group 3
+      Problem: 12.17.4
+      Programmer: [Ethan Bailey]
+      Date: October 23, 2024
+      Version: 1.1  
+      Description: This code conducts radix sort
+      Radix sort is a sorting that sorts the places
+      in order so first it will do the 1's place and
+      sort that then go on to the 10's place and the 
+      100's place to sort the array
+*/
+
 //https://www.youtube.com/watch?v=XiuSW_mEn7g
 
 #include <iostream>
@@ -7,19 +19,21 @@
 
 using namespace std;
 
+// Function to get the digit in a specific place
 int getDigit(int element, int place){
     return (element / place) % 10;
 }
+
 void radixSort(vector<int>& arr){
     for(int place = 1; place <= 100; place *= 10){
-        vector<vector<int>> buckets(10);
+        vector<vector<int>> buckets(10); // creates 10 bucks 0-9 for each digit
 
     for (int i = 0; i < arr.size(); i++){
     int digit = getDigit(arr[i], place);
     buckets[digit].push_back(arr[i]);
 }
 
-arr.clear();
+arr.clear(); // clears original array for next iteration
 
 for(int i = 0; i < buckets.size(); i++){
     for(int j = 0; j < buckets[i].size(); j++){
@@ -32,7 +46,7 @@ for(int i = 0; i < buckets.size(); i++){
 
 int main(){
     vector<int> arr = {110, 32, 79, 221, 555, 901, 343, 627};
-    cout << " Orinial array: " ;
+    cout << "Orinial array: " ;
     for(int i = 0; i  < arr.size(); i++){
         cout << arr[i] << " ";
     }
